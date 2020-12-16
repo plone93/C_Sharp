d6fa7c6929ecaea34af9284d49c089cf1e4e1c63
plone931206 <plone931206@gmail.com>
2020-12-16 14:29:29 +0900

    Callculator

---
+++ WindowsFormsApp_Study1/Calculator.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace WindowsFormsApp_Study1
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Numbutton1 = new System.Windows.Forms.Button();
+            this.Numbutton2 = new System.Windows.Forms.Button();
+            this.NumScreen = new System.Windows.Forms.Label();
+            this.Plusbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Numbutton1
+            // 
+            this.Numbutton1.Location = new System.Drawing.Point(22, 140);
+            this.Numbutton1.Name = "Numbutton1";
+            this.Numbutton1.Size = new System.Drawing.Size(75, 72);
+            this.Numbutton1.TabIndex = 0;
+            this.Numbutton1.Text = "1";
+            this.Numbutton1.UseVisualStyleBackColor = true;
+            this.Numbutton1.Click += new System.EventHandler(this.Numbutton1_Click);
+            // 
+            // Numbutton2
+            // 
+            this.Numbutton2.Location = new System.Drawing.Point(103, 140);
+            this.Numbutton2.Name = "Numbutton2";
+            this.Numbutton2.Size = new System.Drawing.Size(75, 72);
+            this.Numbutton2.TabIndex = 1;
+            this.Numbutton2.Text = "2";
+            this.Numbutton2.UseVisualStyleBackColor = true;
+            this.Numbutton2.Click += new System.EventHandler(this.Numbutton2_Click);
+            // 
+            // NumScreen
+            // 
+            this.NumScreen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.NumScreen.Location = new System.Drawing.Point(22, 13);
+            this.NumScreen.Name = "NumScreen";
+            this.NumScreen.Size = new System.Drawing.Size(1059, 104);
+            this.NumScreen.TabIndex = 2;
+            this.NumScreen.Text = "0";
+            this.NumScreen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Plusbutton
+            // 
+            this.Plusbutton.Location = new System.Drawing.Point(1006, 140);
+            this.Plusbutton.Name = "Plusbutton";
+            this.Plusbutton.Size = new System.Drawing.Size(75, 72);
+            this.Plusbutton.TabIndex = 3;
+            this.Plusbutton.Text = "+";
+            this.Plusbutton.UseVisualStyleBackColor = true;
+            this.Plusbutton.Click += new System.EventHandler(this.Plusbutton_Click);
+            // 
+            // Calculator
+            // 
+            this.ClientSize = new System.Drawing.Size(1105, 535);
+            this.Controls.Add(this.Plusbutton);
+            this.Controls.Add(this.NumScreen);
+            this.Controls.Add(this.Numbutton2);
+            this.Controls.Add(this.Numbutton1);
+            this.Font = new System.Drawing.Font("굴림", 12F);
+            this.Name = "Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Numbutton1;
+        private System.Windows.Forms.Button Numbutton2;
+        private System.Windows.Forms.Label NumScreen;
+        private System.Windows.Forms.Button Plusbutton;
+    }
+}
+
---
+++ WindowsFormsApp_Study1/Calculator.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Study1
+{
+    public partial class Calculator : Form
+    {
+        public int Result = 0;
+        public bool isNewNum = true;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+         
+        private void HelloLabel_Click(object sender, EventArgs e)
+        {
+
+
+            //HelloLabel.Text = "Hello C#";
+
+            int number1 = 1;
+            int number2 = 2;
+
+            int sum = number1 + number2;
+            // HelloLabel.Text = sum.ToString();
+
+            string operator1 = "+";
+            bool isCorrect = true;
+
+
+
+
+        }
+
+        public int Sum(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+        }
+
+        public int Minus(int number1, int number2)
+        {
+            int result = number1 - number2;
+            return result;
+        }
+
+
+        /*
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            //int number1 = Convert.ToInt32(Sum1.Text);
+            //int number2 = Convert.ToInt32(Sum2.Text);
+            int number1 = 0;
+            int number2 = 0;
+
+            String.IsNullOrWhiteSpace(Sum1.Text);//스페이스 검사
+            if (Sum1.Text == "" || Sum2.Text == "")//공백 검사
+            {
+                MessageBox.Show("숫자 입력");
+                Sum1.Focus();
+                return;
+            }
+
+            if (int.TryParse(Sum1.Text, out number1) == false || int.TryParse(Sum2.Text, out number2) == false)//정수로 변환, 실패시
+            {
+                MessageBox.Show("문자가 입력됨, 숫자 입력");
+                Sum1.SelectAll();
+                Sum1.Focus();
+                return;
+            }
+            
+
+            int sum = Sum(number1, number2);
+
+            SumResult.Text = sum.ToString();
+        }
+        */
+        public void SetNum(string num)
+        {
+            if (isNewNum)
+            {
+                NumScreen.Text = num;
+                isNewNum = false;
+            }
+            else if (NumScreen.Text == "0")
+            {
+                NumScreen.Text = num;
+            }
+            else
+            {
+                NumScreen.Text = NumScreen.Text + num;
+            }
+
+        }
+
+        private void Numbutton1_Click(object sender, EventArgs e)
+        {
+            SetNum("1");
+            
+        }
+
+        private void Numbutton2_Click(object sender, EventArgs e)
+        {
+            SetNum("2");
+        }
+
+        private void Plusbutton_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(NumScreen.Text);//정수로 변환
+            Result = Result + num;
+
+            NumScreen.Text = Result.ToString();
+            isNewNum = true;
+        }
+    }
+}
